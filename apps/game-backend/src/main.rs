60880b01eef9d77a44a273c7a620f8823c0f53eb
Refactor Script <refactor-script@daveystruijk.com>
2023-05-10 23:34:17 +0200

    Add websocket handler on the backend

--- apps/game-backend/src/main.rs
+++ apps/game-backend/src/main.rs
@@ -1,48 +1,230 @@
-use axum::{extract::State, http::StatusCode, routing::get, Json, Router, Server};
+use axum::{
+    extract::{
+        ws::{CloseFrame, Message, WebSocket, WebSocketUpgrade},
+        ConnectInfo, State,
+    },
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+    Json, Router, Server,
+};
 use game_postgres_entities::{user::Entity as User, user_item::Entity as UserItem};
-use sea_orm::{ConnectionTrait, Database, DatabaseConnection, EntityTrait};
-use serde_json::{json, Value};
-use std::env;
+use sea_orm::{Database, DatabaseConnection, EntityTrait};
+use serde_json::Value;
+use std::{borrow::Cow, env, net::SocketAddr, ops::ControlFlow};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+//allows to split the websocket stream into separate TX and RX branches
+use futures::{sink::SinkExt, stream::StreamExt};
 
 #[derive(Clone)]
-struct AppState {
+struct AppContext {
     pg: DatabaseConnection,
 }
 
-async fn index(ctx: State<AppState>) -> Result<Json<Value>, (StatusCode, &'static str)> {
-    let response = json!({"status": "ok"}).into();
+async fn index(ctx: State<AppContext>) -> Result<Json<Value>, (StatusCode, &'static str)> {
+    let users = User::find()
+        .into_json()
+        .all(&ctx.pg)
+        .await
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Failed to load users"))?;
 
-    let users = match User::find().all(&ctx.pg).await {
-        Ok(rows) => serde_json::to_string(&rows),
-        Err(_) => return Err((StatusCode::INTERNAL_SERVER_ERROR, "Failed to load users")),
-    };
+    Ok(Json(users.into()))
+}
 
-    Ok(Json(response))
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    println!("{addr} connected.");
+    // finalize the upgrade process by returning upgrade callback.
+    // we can customize the callback by sending additional info such as address.
+    ws.on_upgrade(move |socket| handle_socket(socket, addr))
+}
+
+/// Actual websocket statemachine (one will be spawned per connection)
+async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
+    //send a ping (unsupported by some browsers) just to kick things off and get a response
+    if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
+        println!("Pinged {}...", who);
+    } else {
+        println!("Could not send ping {}!", who);
+        // no Error here since the only thing we can do is to close the connection.
+        // If we can not send messages, there is no way to salvage the statemachine anyway.
+        return;
+    }
+
+    // receive single message from a client (we can either receive or send with socket).
+    // this will likely be the Pong for our Ping or a hello message from client.
+    // waiting for message from a client will block this task, but will not block other client's
+    // connections.
+    if let Some(msg) = socket.recv().await {
+        if let Ok(msg) = msg {
+            if process_message(msg, who).is_break() {
+                return;
+            }
+        } else {
+            println!("client {who} abruptly disconnected");
+            return;
+        }
+    }
+
+    // Since each client gets individual statemachine, we can pause handling
+    // when necessary to wait for some external event (in this case illustrated by sleeping).
+    // Waiting for this client to finish getting its greetings does not prevent other clients from
+    // connecting to server and receiving their greetings.
+    for i in 1..5 {
+        if socket
+            .send(Message::Text(format!("Hi {i} times!")))
+            .await
+            .is_err()
+        {
+            println!("client {who} abruptly disconnected");
+            return;
+        }
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    }
+
+    // By splitting socket we can send and receive at the same time. In this example we will send
+    // unsolicited messages to client based on some sort of server's internal event (i.e .timer).
+    let (mut sender, mut receiver) = socket.split();
+
+    // Spawn a task that will push several messages to the client (does not matter what client does)
+    let mut send_task = tokio::spawn(async move {
+        let n_msg = 20;
+        for i in 0..n_msg {
+            // In case of any websocket error, we exit.
+            if sender
+                .send(Message::Text(format!("Server message {i} ...")))
+                .await
+                .is_err()
+            {
+                return i;
+            }
+
+            tokio::time::sleep(std::time::Duration::from_millis(300)).await;
+        }
+
+        println!("Sending close to {who}...");
+        if let Err(e) = sender
+            .send(Message::Close(Some(CloseFrame {
+                code: axum::extract::ws::close_code::NORMAL,
+                reason: Cow::from("Goodbye"),
+            })))
+            .await
+        {
+            println!("Could not send Close due to {}, probably it is ok?", e);
+        }
+        n_msg
+    });
+
+    // This second task will receive messages from client and print them on server console
+    let mut recv_task = tokio::spawn(async move {
+        let mut cnt = 0;
+        while let Some(Ok(msg)) = receiver.next().await {
+            cnt += 1;
+            // print message and break if instructed to do so
+            if process_message(msg, who).is_break() {
+                break;
+            }
+        }
+        cnt
+    });
+
+    // If any one of the tasks exit, abort the other.
+    tokio::select! {
+        rv_a = (&mut send_task) => {
+            match rv_a {
+                Ok(a) => println!("{} messages sent to {}", a, who),
+                Err(a) => println!("Error sending messages {:?}", a)
+            }
+            recv_task.abort();
+        },
+        rv_b = (&mut recv_task) => {
+            match rv_b {
+                Ok(b) => println!("Received {} messages", b),
+                Err(b) => println!("Error receiving messages {:?}", b)
+            }
+            send_task.abort();
+        }
+    }
+
+    // returning from the handler closes the websocket connection
+    println!("Websocket context {} destroyed", who);
+}
+
+/// helper to print contents of messages to stdout. Has special treatment for Close.
+fn process_message(msg: Message, who: SocketAddr) -> ControlFlow<(), ()> {
+    match msg {
+        Message::Text(t) => {
+            println!(">>> {} sent str: {:?}", who, t);
+        }
+        Message::Binary(d) => {
+            println!(">>> {} sent {} bytes: {:?}", who, d.len(), d);
+        }
+        Message::Close(c) => {
+            if let Some(cf) = c {
+                println!(
+                    ">>> {} sent close with code {} and reason `{}`",
+                    who, cf.code, cf.reason
+                );
+            } else {
+                println!(">>> {} somehow sent close message without CloseFrame", who);
+            }
+            return ControlFlow::Break(());
+        }
+
+        Message::Pong(v) => {
+            println!(">>> {} sent pong with {:?}", who, v);
+        }
+        // You should never need to manually handle Message::Ping, as axum's websocket library
+        // will do so for you automagically by replying with Pong and copying the v according to
+        // spec. But if you need the contents of the pings you can see them here.
+        Message::Ping(v) => {
+            println!(">>> {} sent ping with {:?}", who, v);
+        }
+    }
+    ControlFlow::Continue(())
 }
 
 #[tokio::main]
 async fn start() -> anyhow::Result<()> {
+    // Initialize logging
+    env::set_var("RUST_LOG", "info");
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "game_backend=trace".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    tracing::info!("🟠 Starting up...");
+
+    // Parse environment variables
     dotenvy::dotenv().ok();
+    let listen_addr = env::var("LISTEN_ADDR").unwrap_or("0.0.0.0:4001".to_string());
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
 
     // Initialize database connection
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
     let pg = Database::connect(database_url)
         .await
         .expect("Database connection failed");
 
     // Build app context
-    let state = AppState { pg };
+    let ctx = AppContext { pg };
 
     // Routes
     let app = Router::new()
         .route("/", get(index))
-        .with_state(state)
+        .route("/ws", get(ws_handler))
+        .with_state(ctx)
         .into_make_service();
 
     // Server
-    axum::Server::bind(&"0.0.0.0:1234".parse()?)
-        .serve(app)
-        .await?;
+    let server = Server::bind(&listen_addr.parse()?);
+    tracing::info!("🟢 Listening on {}", listen_addr);
+    server.serve(app).await?;
 
     Ok(())
 }
