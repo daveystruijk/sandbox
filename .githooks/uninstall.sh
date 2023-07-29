d70e6c83bb49d6266ed18fc123bc276cd8727bfd
Davey Struijk <mail@daveystruijk.com>
2023-07-29 13:02:18 +0200

    Replace cdk8s with normal cdk setup

--- .githooks/uninstall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-git config --unset core.hooksPath
---
+++ infra/src/index.ts
@@ -0,0 +1,9 @@
+import * as cdk from 'aws-cdk-lib';
+
+import { InfraStack } from './infra-stack';
+
+const app = new cdk.App();
+
+new InfraStack(app, 'Sandbox', {});
+
+app.synth();
