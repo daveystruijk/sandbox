53141a4d2194c4eaa3120d741bfca06c914c4f82
Davey Struijk <mail@daveystruijk.com>
2023-08-21 09:19:42 +0200

    chore: update eslint-config and dependencies, add @typescript-eslint/recommended-type-checked

--- libs/eslint-config/index.js
+++ libs/eslint-config/index.js
@@ -5,8 +5,11 @@ module.exports = {
     node: true,
   },
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'prettier'],
 
   rules: {
     // Error on unused vars, but not if they have a leading underscore
