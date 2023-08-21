78276314f9d807f9cec5ebab57bc99dba129d6b5
Davey Struijk <mail@daveystruijk.com>
2023-08-22 01:16:12 +0200

    chore: add package checker script

---
+++ scripts/verify-package-scripts-exist.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env tsx
+import { resolve } from 'path';
+import { findWorkspacePackages, Project } from '@pnpm/workspace.find-packages';
+
+const workspaceRoot = resolve(__dirname, '..');
+const fieldsToLookFor = ['dev', 'test', 'lint'];
+
+const getMissingScripts = (project: Project) => {
+  if (!project.manifest.scripts) {
+    return fieldsToLookFor;
+  }
+  const scripts = Object.keys(project.manifest.scripts);
+  return scripts.filter((key) => key in fieldsToLookFor);
+};
+
+export const verifyPackageScriptsExist = async () => {
+  const projects = await findWorkspacePackages(workspaceRoot);
+  for (const project of projects) {
+    const missingScripts = getMissingScripts(project);
+    console.log(`${project.dir}: ${missingScripts}`);
+  }
+};
+
+if (require.main === module) {
+  verifyPackageScriptsExist();
+}
